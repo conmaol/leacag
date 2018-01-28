678784a507a953453f4dc47f71dffe66c56cb13c
Mark McConville <Mark.McConville@glasgow.ac.uk>
2018-01-28 22:01:52 +0000

    Update index.php
    Added lang fields for all the other forms.

--- index.php
+++ index.php
@@ -193,10 +193,11 @@ header("Expires: 0"); // Proxies.
                           Add English equivalent term:
                       </h3>
                       <p>
-                          <textarea name="comment" class="formField"></textarea>
+                          <textarea name="trans" class="formField"></textarea>
                       </p>
                       <p>
                           <input type="hidden" name="action" value="processAddEnglishForm"/>
+                          <input type="hidden" name="lang" value="gd"/>
                           <input type="hidden" name="id" id="lexId"/>
                           <input type="hidden" name="userEmail" class="userEmail"/>
                           <button class="popupClose">cuir às</button>
@@ -213,10 +214,11 @@ header("Expires: 0"); // Proxies.
                           Add orthographic form:
                       </h3>
                       <p>
-                          <textarea name="comment" class="formField"></textarea>
+                          <textarea name="target" class="formField"></textarea>
                       </p>
                       <p>
                           <input type="hidden" name="action" value="processAddFormOrthForm"/>
+                          <input type="hidden" name="lang" value="gd"/>
                           <input type="hidden" name="id" id="lexId"/>
                           <input type="hidden" name="userEmail" class="userEmail"/>
                           <button class="popupClose">cuir às</button>
@@ -233,10 +235,11 @@ header("Expires: 0"); // Proxies.
                           Authorise English equivalent term:
                       </h3>
                       <p>
-                          <textarea name="comment" class="formField"></textarea>
+                          <textarea name="trans" class="formField"></textarea>
                       </p>
                       <p>
                           <input type="hidden" name="action" value="processAuthEnglishForm"/>
+                          <input type="hidden" name="lang" value="gd"/>
                           <input type="hidden" name="id" id="lexId"/>
                           <input type="hidden" name="userEmail" class="userEmail"/>
                           <button class="popupClose">cuir às</button>
